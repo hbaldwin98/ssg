b02217b1b6a99bba957408d15a4a23a58edc5123
hunter.baldwin <huntercbaldwin98@gmail.com>
2024-06-04 06:39:50 -0700

    Added parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,7 +4,7 @@ class HTMLNode:
         tag: str = None,
         value: str = None,
         children: list = None,
-        props: dict = None
+        props: dict = None,
     ):
         self.tag = tag
         self.value = value
@@ -16,9 +16,9 @@ class HTMLNode:
 
     def props_to_html(self):
         if self.props is None:
-            return ''
+            return ""
 
-        props_html = ''
+        props_html = ""
 
         for key, value in self.props.items():
             props_html += f' {key}="{value}"'
@@ -26,40 +26,49 @@ class HTMLNode:
         return props_html.strip()
 
     def __repr__(self):
-        output = ''
-        output += f'Tag: {self.tag}\nValue: {self.value}'
-
-        if self.children is not None:
-            output += '\nChildren:\n'
-            for child in self.children:
-                print(child)
-
-        if self.props is not None:
-            output += '\nProps:'
-            for key, value in self.props.items():
-                output += f'\n{key}: {value}\n'
-
-        return output
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
 
 
 class LeafNode(HTMLNode):
-    def __init__(self,
-                 tag: str = None,
-                 value: str = None,
-                 props: dict = None
-                 ):
+    def __init__(self, tag: str = None, value: str = None, props: dict = None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value is None:
-            raise ValueError('Leaf node requires a value')
+            raise ValueError("Leaf node requires a value")
 
         if self.tag is None:
             return self.value
 
-        output = f'<{self.tag}'
+        output = f"<{self.tag}"
 
         if self.props is not None:
-            output += f' {self.props_to_html()}'
+            output += f" {self.props_to_html()}"
 
-        return f'{output}>{self.value}</{self.tag}>'
+        return f"{output}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children, props: dict = None):
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag cannot be None")
+
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("Must have at least one child")
+
+        output = f"<{self.tag}"
+
+        if self.props is not None:
+            output += f" {self.props_to_html()}"
+
+        output += ">"
+
+        for child in self.children:
+            output += child.to_html()
+
+        return output + f"</{self.tag}>"
---
+++ src/test_parentnode.py
@@ -0,0 +1,41 @@
+import unittest
+
+from htmlnode import ParentNode, LeafNode
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_when_children_is_none(self):
+        node = ParentNode("p", None, {"prop": "value"})
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_with_single_child(self):
+        child_node = LeafNode("strong", "this is some text", None)
+        node = ParentNode("p", [child_node], {"style": "margin: 1rem"})
+
+        self.assertEqual(
+            node.to_html(),
+            '<p style="margin: 1rem"><strong>this is some text</strong></p>',
+        )
+
+    def test_to_html_with_two_children(self):
+        child_node = LeafNode("strong", "this is some text", None)
+        child_node_2 = LeafNode("p", "this is some text as well", {"color": "red"})
+        node = ParentNode("p", [child_node, child_node_2], {"style": "margin: 1rem"})
+
+        self.assertEqual(
+            node.to_html(),
+            '<p style="margin: 1rem"><strong>this is some text</strong><p color="red">this is some text as well</p></p>',
+        )
+
+    def test_to_html_with_nested_parent(self):
+        child_node = LeafNode("strong", "this is some text", None)
+        child_node_2 = LeafNode("span", "this is some text as well", {"color": "red"})
+        nested_parent_node = ParentNode("p", [child_node_2], None)
+        node = ParentNode(
+            "p", [child_node, nested_parent_node], {"style": "margin: 1rem"}
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            '<p style="margin: 1rem"><strong>this is some text</strong><p><span color="red">this is some text as well</span></p></p>',
+        )
