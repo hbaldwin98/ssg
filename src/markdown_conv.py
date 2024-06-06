d455e825d1cc13bea0798211744cf71774dfc437
hunter.baldwin <huntercbaldwin98@gmail.com>
2024-06-06 06:32:58 -0700

    Added markdown conversion to nodes - refactored

---
+++ src/markdown_conv.py
@@ -0,0 +1,177 @@
+from typing import List, Tuple
+import re
+from htmlnode import HTMLNode, LeafNode
+from textnode import TextNode, TextType
+
+
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+    match text_node.text_type:
+        case TextType.Text:
+            return LeafNode(None, text_node.text)
+        case TextType.Bold:
+            return LeafNode("strong", text_node.text)
+        case TextType.Italic:
+            return LeafNode("em", text_node.text)
+        case TextType.Code:
+            return LeafNode("code", text_node.text)
+        case TextType.Link:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.Image:
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError("Invalid text_node type supplied.")
+
+
+def split_nodes_delimiter(
+    old_nodes: List[TextNode], delimiter: str, text_type: TextType
+) -> List[HTMLNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.Text:
+            new_nodes.append(node)
+            continue
+
+        if delimiter not in node.text:
+            raise Exception(
+                f"ERROR! Node with text: {node.text}\nInvalid markdown syntax."
+            )
+
+        split_values = node.text.split(delimiter)
+
+        if len(split_values) % 2 == 0:
+            raise ValueError("Invalid Markdown: Formatted section not closed")
+        split_nodes = []
+
+        for i in range(len(split_values)):
+            if len(split_values[i]) == 0:
+                continue
+
+            if i % 2 == 0:
+                split_nodes.append(TextNode(split_values[i], TextType.Text))
+            else:
+                split_nodes.append(TextNode(split_values[i], text_type))
+
+        new_nodes.extend(split_nodes)
+
+    return new_nodes
+
+
+def split_nodes_links(old_nodes: List[TextNode]) -> List[HTMLNode]:
+    def split_recurse(text: str) -> List[str]:
+        if text == "":
+            return []
+
+        image_tup = extract_markdown_links(text)
+        if len(image_tup) == 0:
+            return [text]
+
+        result = []
+        split = text.split(f"[{image_tup[0][0]}]({image_tup[0][1]})", 1)
+
+        if len(split) > 0 and split[0] != "":
+            result.append(split[0])
+
+        result.append(f"[{image_tup[0][0]}]({image_tup[0][1]})")
+
+        if len(split) > 1 and split[1] != "":
+            result.extend(split_recurse(split[1]))
+
+        return result
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.Text:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+
+        split_values = split_recurse(original_text)
+        if len(split_values) == 0:
+            new_nodes.append(node)
+            continue
+
+        result = []
+        for value in split_values:
+            if value == "":
+                continue
+
+            image = extract_markdown_links(value)
+            if len(image) > 0:
+                result.append(TextNode(image[0][0], TextType.Link, image[0][1]))
+            else:
+                result.append(TextNode(value, TextType.Text))
+
+        new_nodes.extend(result)
+
+    return new_nodes
+
+
+def split_nodes_images(old_nodes: List[TextNode]) -> List[HTMLNode]:
+    def split_recurse(text: str) -> List[str]:
+        if text == "":
+            return []
+
+        image_tup = extract_markdown_images(text)
+        if len(image_tup) == 0:
+            return [text]
+
+        result = []
+        split = text.split(f"![{image_tup[0][0]}]({image_tup[0][1]})", 1)
+
+        if len(split) > 0 and split[0] != "":
+            result.append(split[0])
+
+        result.append(f"![{image_tup[0][0]}]({image_tup[0][1]})")
+
+        if len(split) > 1 and split[1] != "":
+            result.extend(split_recurse(split[1]))
+
+        return result
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.Text:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+
+        split_values = split_recurse(original_text)
+        if len(split_values) == 0:
+            new_nodes.append(node)
+            continue
+
+        result = []
+        for value in split_values:
+            if value == "":
+                continue
+
+            image = extract_markdown_images(value)
+            if len(image) > 0:
+                result.append(TextNode(image[0][0], TextType.Image, image[0][1]))
+            else:
+                result.append(TextNode(value, TextType.Text))
+
+        new_nodes.extend(result)
+
+    return new_nodes
+
+
+def extract_markdown_images(text: str) -> Tuple[str, str]:
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text: str) -> Tuple[str, str]:
+    return re.findall(r"(?<!\!)\[(.*?)\]\((.*?)\)", text)
+
+
+def markdown_to_nodes(text: str) -> List[TextNode]:
+    nodes = [TextNode(text, TextType.Text)]
+    nodes.extend(split_nodes_delimiter(nodes, "**", TextType.Bold))
+    nodes.extend(split_nodes_delimiter(nodes, "*", TextType.Italic))
+    nodes.extend(split_nodes_delimiter(nodes, "`", TextType.Code))
+    nodes.extend(split_nodes_images(nodes))
+    nodes.extend(split_nodes_links(nodes))
+
+    return nodes
---
+++ src/test_markdown_conv.py
@@ -0,0 +1,244 @@
+from markdown_conv import (
+    extract_markdown_images,
+    split_nodes_images,
+    split_nodes_links,
+    split_nodes_delimiter,
+)
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class ExtractLinksAndImages(unittest.TestCase):
+    def test_splits_nodes(self):
+        test_cases = [
+            (
+                TextNode("*Test*", TextType.Text),
+                "*",
+                TextType.Bold,
+                [TextNode("Test", TextType.Bold)],
+            ),
+            (
+                TextNode("test *Test*", TextType.Text),
+                "*",
+                TextType.Bold,
+                [TextNode("test ", TextType.Text),
+                 TextNode("Test", TextType.Bold)],
+            ),
+            (
+                TextNode("test *Test* test", TextType.Text),
+                "*",
+                TextType.Bold,
+                [
+                    TextNode("test ", TextType.Text),
+                    TextNode("Test", TextType.Bold),
+                    TextNode(" test", TextType.Text),
+                ],
+            ),
+            (
+                TextNode("This is text with a `code block` word", TextType.Text),
+                "`",
+                TextType.Code,
+                [
+                    TextNode("This is text with a ", TextType.Text),
+                    TextNode("code block", TextType.Code),
+                    TextNode(" word", TextType.Text),
+                ],
+            ),
+        ]
+
+        for case in test_cases:
+            result = split_nodes_delimiter([case[0]], case[1], case[2])
+
+            self.assertTrue(len(result) == len(case[3]))
+
+            for i in range(len(case[3])):
+                self.assertTrue(result[i].text == case[3][i].text)
+                self.assertTrue(result[i].url == case[3][i].url)
+                self.assertTrue(result[i].text_type == case[3][i].text_type)
+
+    def MarkdownConversionTests(self):
+        test_cases = [
+            (
+                "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)",
+                (
+                    [
+                        (
+                            "image",
+                            "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+                        ),
+                        (
+                            "another",
+                            "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+                        ),
+                    ]
+                ),
+            )
+        ]
+
+        for test_case in test_cases:
+            output = extract_markdown_images(test_case[0])
+            for i in range(len(output)):
+                self.assertTrue(output[i][0] == test_case[1][i][0])
+                self.assertTrue(output[i][1] == test_case[1][i][1])
+
+    def test_extract_links(self):
+        test_cases = [
+            (
+                "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)",
+                (
+                    [
+                        (
+                            "link",
+                            "https://www.example.com",
+                        ),
+                        (
+                            "another",
+                            "https://www.example.com/another",
+                        ),
+                    ]
+                ),
+            )
+        ]
+
+        for test_case in test_cases:
+            output = extract_markdown_images(test_case[0])
+            for i in range(len(output)):
+                self.assertTrue(output[i][0] == test_case[1][i][0])
+                self.assertTrue(output[i][1] == test_case[1][i][1])
+
+    def test_split_images(self):
+        test_cases = [
+            (
+                TextNode(
+                    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)",
+                    TextType.Text,
+                ),
+                [
+                    TextNode("This is text with an ", TextType.Text),
+                    TextNode(
+                        "image",
+                        TextType.Image,
+                        "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+                    ),
+                    TextNode(" and ", TextType.Text),
+                    TextNode(
+                        "another",
+                        TextType.Image,
+                        "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+                    ),
+                ],
+            ),
+            (
+                TextNode("No image here folks", TextType.Text),
+                [TextNode("No image here folks", TextType.Text)],
+            ),
+            (
+                TextNode("![image](https://example.com/)", TextType.Text),
+                [TextNode("image", TextType.Image, "https://example.com/")],
+            ),
+            (
+                TextNode(
+                    "![image](https://example.com/) ![another one](https://example.com/)",
+                    TextType.Text,
+                ),
+                [
+                    TextNode("image", TextType.Image, "https://example.com/"),
+                    TextNode(" ", TextType.Text),
+                    TextNode("another one", TextType.Image,
+                             "https://example.com/"),
+                ],
+            ),
+            (
+                TextNode("[link][https://noimageherefolks.com/]",
+                         TextType.Text),
+                [TextNode("[link][https://noimageherefolks.com/]", TextType.Text)],
+            ),
+        ]
+
+        for test_case in test_cases:
+            result = split_nodes_images([test_case[0]])
+            for i in range(len(test_case[1])):
+                self.assertTrue(
+                    test_case[1][i].text == result[i].text,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
+                self.assertTrue(
+                    test_case[1][i].text_type == result[i].text_type,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
+                self.assertTrue(
+                    test_case[1][i].url == result[i].url,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
+
+    def test_split_links(self):
+        test_cases = [
+            (
+                TextNode(
+                    "This is text with an [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and [another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)",
+                    TextType.Text,
+                ),
+                [
+                    TextNode("This is text with an ", TextType.Text),
+                    TextNode(
+                        "link",
+                        TextType.Link,
+                        "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+                    ),
+                    TextNode(" and ", TextType.Text),
+                    TextNode(
+                        "another",
+                        TextType.Link,
+                        "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+                    ),
+                ],
+            ),
+            (
+                TextNode("No link here folks", TextType.Text),
+                [TextNode("No link here folks", TextType.Text)],
+            ),
+            (
+                TextNode("[image](https://example.com/)", TextType.Text),
+                [TextNode("image", TextType.Link, "https://example.com/")],
+            ),
+            (
+                TextNode(
+                    "[image](https://example.com/) [another one](https://example.com/)",
+                    TextType.Text,
+                ),
+                [
+                    TextNode("image", TextType.Link, "https://example.com/"),
+                    TextNode(" ", TextType.Text),
+                    TextNode("another one", TextType.Link,
+                             "https://example.com/"),
+                ],
+            ),
+            (
+                TextNode(
+                    "![image](https://example.com/) [another one](https://example.com/)",
+                    TextType.Text,
+                ),
+                [
+                    TextNode("![image](https://example.com/) ", TextType.Text),
+                    TextNode("another one", TextType.Link,
+                             "https://example.com/"),
+                ],
+            ),
+        ]
+
+        for test_case in test_cases:
+            result = split_nodes_links([test_case[0]])
+            for i in range(len(test_case[1])):
+                self.assertTrue(
+                    test_case[1][i].text == result[i].text,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
+                self.assertTrue(
+                    test_case[1][i].text_type == result[i].text_type,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
+                self.assertTrue(
+                    test_case[1][i].url == result[i].url,
+                    f"Result: {result[i]}, Expected: {test_case[1][i]}",
+                )
